876580df5e10a59dbad0b2c36f9de51b57b40f37
Ali Fatolahi <ali.fatolahi@gmail.com>
2025-05-29 15:56:25 -0400

    initial application code

---
+++ src/main/java/com/daya/ws/symposium/DayaMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.daya.ws.symposium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayaMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayaMicroserviceApplication.class, args);
+	}
+
+}
